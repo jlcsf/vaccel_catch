78276d5975d1598640ad483b1fd9a3774aaa74b7
jlcsf <143977754+jlcsf@users.noreply.github.com>
2023-11-08 03:29:11 +0000

    classify test

--- make_script.sh
+++ make_script.sh
@@ -6,8 +6,10 @@ mkdir -p build
 cd build
 cmake ../ -DBUILD_PLUGIN_NOOP=ON -DENABLE_TESTS=ON -DBUILD_EXAMPLES=ON
 make
-# make test ARGS="-V"
-make CTEST_OUTPUT_ON_FAILURE=1 test
+make test ARGS="-V"
+# make CTEST_OUTPUT_ON_FAILURE=1 test
 
 cd ../
 sh ./run_code_coverage.sh
+
+
--- src/vaccel.h
+++ src/vaccel.h
@@ -15,7 +15,7 @@
 #ifndef __VACCEL_H__
 #define __VACCEL_H__
 
-#define VACCELRT_VERSION "df4068b-dirty"
+#define VACCELRT_VERSION "c00029e-dirty"
 
 #include <stdint.h>
 #include <stddef.h>
