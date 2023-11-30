10dbd91f9713a96dd4c90848dcb9ae1c1518bc6f
jlcsf <jlcslvtwrk@gmail.com>
2023-11-30 19:11:02 +0000

    redo...

--- test/test_plugin.cpp
+++ test/test_plugin.cpp
@@ -23,38 +23,40 @@ static int init(void)
     return VACCEL_OK;
 }
 
-static int no_op_exec(){return 2;}
+static int no_op(){return 2;}
 
-TEST_CASE("plugin_test_basic_test", "[plugin_tests]")
-{
-    struct vaccel_plugin plugin;
-    struct vaccel_plugin_info pinfo;
-    plugin.info = &pinfo;
-    plugin.info->name = pname;
-    list_init_entry(&plugin.entry);
-    list_init_entry(&plugin.ops);
-    plugin.info->init = init;
-    plugin.info->fini = fini;
+TEST_CASE("get_all_available_functions") {
 
+    vaccel_plugin no_op_plugin;
+    vaccel_plugin_info noop_pinfo;
     int ret;
-    ret = plugins_bootstrap();
+
+    vaccel_op noop_operation;
+    noop_operation.type = VACCEL_NO_OP;
+    noop_operation.func = (void *)no_op;
+    noop_operation.owner = nullptr;
+
+    no_op_plugin.info = &noop_pinfo;
+    no_op_plugin.info->name = pname;
+    list_init_entry(&no_op_plugin.entry);
+    list_init_entry(&no_op_plugin.ops);
+    no_op_plugin.info->init = init;
+    no_op_plugin.info->fini = fini;
+
+    plugins_bootstrap();
+
+    noop_operation.owner = &no_op_plugin;
+    ret = register_plugin(&no_op_plugin);
+    REQUIRE(ret ==  VACCEL_OK);
+
+    ret = register_plugin_function(&noop_operation);
+    REQUIRE(ret ==  VACCEL_OK);
+
+    ret = get_available_plugins(VACCEL_NO_OP);
     REQUIRE(ret == VACCEL_OK);
 
-    ret = register_plugin(&plugin);
+    ret = unregister_plugin(&no_op_plugin);
     REQUIRE(ret == VACCEL_OK);
 
-    struct vaccel_op exec_operation;
-    exec_operation.type = VACCEL_EXEC;
-    exec_operation.func = (void *)no_op_exec;
-    exec_operation.owner = &plugin;
-
-    ret = register_plugin_function(&exec_operation);
-    REQUIRE(ret == VACCEL_OK);
-
-    // ret = unregister_plugin(&plugin);
-    // REQUIRE(ret == VACCEL_OK);
-
-    ret = plugins_shutdown();
-    REQUIRE(ret == VACCEL_OK);
-
+    plugins_shutdown();
 }
